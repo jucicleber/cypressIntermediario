75639ed77557076fc7910df2fc57272ee6f3f861
Jucicleber da Silva Gomes <71895344+jucicleber@users.noreply.github.com>
2025-01-22 20:14:31 -0400

    curso cypress intermediario

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  
+    e2e:{
+      baseUrl:'http://localhost'
+    },
+    fixturesFolder: false,
+    video:false,
+  
+})
